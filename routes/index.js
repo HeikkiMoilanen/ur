9549cb093c426557a6384ff17a03e5fa2c67b94c
Johannes <johannes.rauhamaa@aalto.fi>
2016-02-27 17:27:32 +0200

    Successfully redirect

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var urlModel = require("../backend/models/urls");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.sendFile(path.resolve(__dirname + '/../frontend/index.html'));
+});
+
+router.get('/:shortURL', function(req, res, next) {
+	urlModel.find({_id: req.params.shortURL}, function(err, results){
+		if(err) {
+			// do something here
+			return;
+		}
+		if(results.length === 0){
+			// 404
+			return;
+		}
+		res.redirect(results[0].longUrl);
+	});
 });
 
 module.exports = router;
