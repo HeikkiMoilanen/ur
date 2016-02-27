ae5b1f55027b442e0a8739521944d1026161d28d
HeikkiMoilanen <moiheikki@gmail.com>
2016-02-27 18:08:48 +0200

    http response messages should be descriptive

--- routes/api.js
+++ routes/api.js
@@ -29,15 +29,21 @@ router.post('/', function(req, res, next) {
 });
 
 router.get('/:shortUrl', function (req, res, next) {
-	var shortUrl = req.params.shortUrl
+  var shortUrl = req.params.shortUrl;
 
-	urlModel.find({'_id': shortUrl}, function(err, data) {
-		if(err || data.length == 0 || !data[0].longUrl) {
-			res.status(404).send('not found');
-		} else {
-			res.json({message: data[0].longUrl})
-		}
-	});
+  urlModel.find({'_id': shortUrl}, function(err, data) {
+    if(err) {
+      res.status(err.status || 500);
+      var errorMessage = err.message || 'database error';
+      res.json({ message: errorMessage });
+    } else if(data.length == 0) {
+      res.status(404).json({ message: 'not found'} );
+    } else if (!data[0].longUrl) {
+      res.status(500).json({ message: 'no long url by that id' });
+    } else {
+      res.json({message: data[0].longUrl});
+    }
+  });
 });
 
 module.exports = router;
